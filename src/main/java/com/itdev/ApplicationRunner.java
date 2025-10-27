db58d4887d7e8c0ce5546155822c0119cd262e9b
IiLuha <113338886+IiLuha@users.noreply.github.com>
2025-10-27 18:25:11 +0300

    Revert "Final PR DevToMain"

--- src/main/java/com/itdev/ApplicationRunner.java
+++ src/main/java/com/itdev/ApplicationRunner.java
@@ -1,14 +1,2 @@
-package com.itdev;
-
-import com.itdev.listener.OperationsConsoleListener;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class ApplicationRunner {
-
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext("com.itdev");
-        OperationsConsoleListener listener = context.getBean(OperationsConsoleListener.class);
-        listener.doListen(args);
-    }
+package com.itdev;public class ApplicationRunner {
 }
